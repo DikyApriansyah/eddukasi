b7004acd5244b8f6b41a1cb9e22cd00834394365
LENOVO <dikyapriansyah25@gmail.com>
2022-05-11 23:30:49 +0800

    finish

---
+++ app/src/main/java/com/example/edukasianak/Aqidah.java
@@ -0,0 +1,14 @@
+package com.example.edukasianak;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Aqidah extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_aqidah);
+    }
+}
---
+++ app/src/main/java/com/example/edukasianak/akhlak.java
@@ -0,0 +1,14 @@
+package com.example.edukasianak;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class akhlak extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_akhlak);
+    }
+}
